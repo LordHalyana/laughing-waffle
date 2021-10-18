53b74aa67c73c360017d804fa06bec17ab583040
LordHalyana <1707310+LordHalyana@users.noreply.github.com>
2021-10-18 16:13:11 +0200

    button

---
+++ about/about.js
@@ -0,0 +1,8 @@
+function showDiscord() {
+    var x = document.getElementById("discordID");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
